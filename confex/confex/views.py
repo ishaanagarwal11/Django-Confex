bd931ca9291231e49e86d8e5abb97eca1516a588
Ishaan Agarwal <ishaanagarwal.uni@gmail.com>
2024-03-29 15:00:54 +0530

    basics
    all the basic functionalities built
    //next is styling

---
+++ confex/confex/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def home(request):
+   # return HttpResponse('This is the HOME page')
+    return render(request, 'home.html')
+
+def about(request):
+  #  return HttpResponse('This is the about page')
+    return render(request, 'about.html')
